f878c864a0feefffcf0cebc20591b2f94225d2ae
Robert McCarthy <robbiemccarthy0@gmail.com>
2025-05-18 21:38:48 +0100

    Fix: use env variables for email auth correctly

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -32,10 +32,11 @@ export async function POST(req) {
 
   const transporter = nodemailer.createTransport({
     service: 'gmail',
-    auth: {
+   auth: {
   user: process.env.EMAIL_USER,
   pass: process.env.EMAIL_PASS,
 }
+
 ,
   });
 
