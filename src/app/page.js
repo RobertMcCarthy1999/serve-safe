6658b23bcec0b37e7ed5ecca764821a4a7f89e34
Robert McCarthy <robbiemccarthy0@gmail.com>
2025-05-20 20:24:49 +0100

    Fix NavBar and Footer import path in upload/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
-import NavBar from './components/NavBar';
-import Footer from './components/footer';
+import NavBar from '../components/NavBar';
+import Footer from '../components/footer';
 
 export default function HomePage() {
   return (
