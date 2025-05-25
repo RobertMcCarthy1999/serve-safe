37339296ba867b13bfd4b00abb86f22c22f5bff1
Robert McCarthy <robbiemccarthy0@gmail.com>
2025-05-25 16:47:34 +0100

    Protect /startsafe/send with client-side Supabase login check

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'], // <- Critical for App Router
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
