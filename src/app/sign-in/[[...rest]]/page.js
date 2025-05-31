604a5b623e6df870e9d641547315fc0548d520c2
Robert McCarthy <robbiemccarthy0@gmail.com>
2025-05-31 21:06:15 +0100

    Center Clerk SignIn component and style update

--- src/app/sign-in/[[...rest]]/page.js
+++ src/app/sign-in/[[...rest]]/page.js
@@ -3,8 +3,10 @@ import { SignIn } from '@clerk/nextjs';
 
 export default function SignInPage() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <SignIn redirectUrl="/dashboard" />
+    <div className="flex items-center justify-center min-h-screen bg-white px-4">
+      <div className="w-full max-w-md">
+        <SignIn redirectUrl="/dashboard" />
+      </div>
     </div>
   );
 }
