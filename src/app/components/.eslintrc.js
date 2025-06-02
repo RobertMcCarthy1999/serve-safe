6c19f0b9df8458210068eb741f2f3b316ba98880
Robert McCarthy <robbiemccarthy0@gmail.com>
2025-06-02 20:11:41 +0100

    Remove duplicate .eslintrc.js from src/app to fix ESLint config scope

--- src/app/components/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-// .eslintrc.js
-module.exports = {
-  root: true,
-  extends: ['next', 'next/core-web-vitals', 'eslint:recommended'],
-  rules: {
-    // Example: turn off rule if needed
-    // 'no-console': 'warn',
-  },
-};
