5d4308514e030ac24f111236793ef2a053dd2c38
Robert McCarthy <robbiemccarthy0@gmail.com>
2025-06-02 15:47:45 +0100

    Fix dynamic import for ReactToPrint to prevent SSR crash

--- src/app/inventorypro/send/page.js
+++ src/app/inventorypro/send/page.js
@@ -1,6 +1,9 @@
 'use client';
 import React, { useState, useRef } from 'react';
-import ReactToPrint from 'react-to-print';
+import dynamic from 'next/dynamic';
+
+const ReactToPrint = dynamic(() => import('react-to-print'), { ssr: false });
+
 
 
 export default function InventoryProSendPage() {
